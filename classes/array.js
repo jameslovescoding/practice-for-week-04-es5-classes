c333e65c53ae5c570fa9bed51fa15e1361211127
James Yu Cao <caoyu19920414@gmail.com>
2023-04-14 20:16:30 -0400

    finished all

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,13 @@
 // Your code here
+
+Array.prototype.isEqual = function(arr) {
+    if (this.length !== arr.length) {
+        return false;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (this[i] !== arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
